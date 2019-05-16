a691809aeb62be1e1fe4423fb56cc82a9e8df917
ariefhirmanto <ariefhirmanto7@gmail.com>
2019-05-16 15:39:34 +0700

    push scrolling section

---
+++ index.js
@@ -0,0 +1,21 @@
+//import iOs.js
+
+
+//import a library
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+//create a component
+const App = () => 
+	(
+		<View style={{ flex: 1 }}>
+			<Header headerText={'Albums'} />
+			<AlbumList />
+		</View>
+	);
+
+////render
+AppRegistry.registerComponent('albums', () => App);
+
